8aca35c1502090c2afed7666abc825cb9313c520
Saiiiii42803 <guna.koganti@gmail.com>
2026-07-13 11:30:18 -0500

    Lesseon 2

---
+++ m1/L2/L2_print_statements.py
@@ -0,0 +1,12 @@
+# printing the sentence hello world
+print("hello world")
+
+# printing a number
+print(12345)
+
+# printing multiple arguments
+print("hello world", 12345)
+
+# End argument in print function
+print("Welcome to codingal", end=".")
+
