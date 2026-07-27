a491f8f0d096847141fb910a690bb7f1397a7c23
Saiiiii42803 <guna.koganti@gmail.com>
2026-07-27 11:49:15 -0500

    membership

---
+++ m1/L7/membership.py
@@ -0,0 +1,16 @@
+a = "pineapple"
+
+print("apple" in a)
+print("apples" in a)
+
+print("apple" not in a)
+print("apples" not in a)
+
+
+b = [1, 2, 3, 4, 5]
+
+print(1 in b)
+print(6  in b)
+
+print(1 not in b)
+print(6 not in b)
