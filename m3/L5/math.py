de1fd7a06ae1f44f83879dfe1a0422dda4c18985
Saiiiii42803 <guna.koganti@gmail.com>
2026-08-12 21:44:05 -0500

    Math

---
+++ m3/L5/math.py
@@ -0,0 +1,21 @@
+import math
+
+'''
+.ceil()
+.floor()
+.sqrt()
+.gcd()
+.fabs()
+'''
+
+test1 = 45.1
+test2 = 45.1
+test3 = 100
+test4 = 24, 8
+test5 = -50
+
+print(math.ceil(test1))
+print(math.floor(test2))
+print(math.sqrt(test3))
+print(math.gcd(24, 8))
+print(math.fabs(test5))
