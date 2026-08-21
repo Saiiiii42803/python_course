6810cf65304fbd0e4a4305a14dfac83afc69ec90
Saiiiii42803 <guna.koganti@gmail.com>
2026-08-20 21:37:51 -0500

    palindromes

---
+++ m4/L2/palindome.py
@@ -0,0 +1,12 @@
+print("-----Make a paindrome-----")
+
+one = int(input("enter a number: "))
+two = int(input("enter a number: "))
+three = int(input("enter a number: "))
+
+tbl = (one, two, three)
+
+if one == three:
+    print("you made a palindrome")
+else:
+    print(" try again EX: 121")
