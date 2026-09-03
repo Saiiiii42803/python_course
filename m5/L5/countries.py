b69bb0c4ab2d73ec9ea07b76d378b4ed1b345e1b
Saiiiii42803 <guna.koganti@gmail.com>
2026-09-02 21:30:51 -0500

    Polymorphism

---
+++ m5/L5/countries.py
@@ -0,0 +1,13 @@
+class India():
+    def capital(self):
+        print("New deli is the capital of india")
+
+class Usa():
+    def capital(self):
+        print("Washington D.C. is the capital of the USA")
+
+i = India()
+u = Usa()
+
+i.capital()
+u.capital()
